48dce343272252b755f1be977f9ebcff0eb9c186
Gerrit Janßen <gerrit.janssen@mailbox.tu-berlin.de>
2018-01-18 16:57:28 +0100

    changed image tag

--- pipeline_interfaces/java_project/pipeline/deploy_docker.sh
+++ pipeline_interfaces/java_project/pipeline/deploy_docker.sh
@@ -2,5 +2,5 @@
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t pipeline_java .
 docker images
-docker tag pipeline_java cloudproto/pipeline_java
-docker push cloudproto/pipeline_java
+docker tag pipeline_java cloudproto/pipeline_component
+docker push cloudproto/pipeline_component
